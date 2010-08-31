84e275305258eecd73bd7c5267579709e93cabec
alexandre.cyprien <alexandre.cyprien@8e971d8e-9cf3-0c36-aa0f-a7c54ab41ffc>
2010-08-31 17:47:17 +0000

    alexandre cyprien _________________
    ADD : widget center, handle the list of widget and distribute the notification to the right widget
    MODIF : Notification to add new attribute
    	Gesture.py => main script, launch 2 threads allowing the communication between the gesture center/notifcication center and the widget center

--- trunk/Gesture/Gesture.py
+++ trunk/Gesture/Gesture.py
@@ -1,4 +1,31 @@
-from GestureCenter.Gestures import *
+from GestureCenter.GestureCenter import *
 from GestureNotificationCenter.GestureNotificationCenter import *
-import socket
+from WidgetCenter.WidgetCenter import *
+import time
+import threading
 
+i = 0;
+
+g_center = GestureCenter()
+w_center = WidgetCenter()
+center = MoveNotificationCenter()
+
+
+def start_GestureCenter(g_center, center):
+	while (True):
+		print ""
+		
+
+def start_WidgetCenter(w_center, center):
+	while (True):
+		notif = center.handleNotification()
+		w_center.examinNotif(notif)
+		if notif is None:
+			print "No notification"
+		else :
+			print notif.getAssociateGestureId(), notif.getLabel()
+
+thread_a = threading.Thread(None,start_GestureCenter, None, (g_center,center,))
+thread_b = threading.Thread(None,start_WidgetCenter, None, (w_center,center,))
+thread_a.start()
+thread_b.start()
