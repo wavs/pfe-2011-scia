1a167497383482b2f734f79a772e3a7afec3186c
alexandre.cyprien <alexandre.cyprien@8e971d8e-9cf3-0c36-aa0f-a7c54ab41ffc>
2010-09-01 16:45:21 +0000

    alexandre cyprien ________________
    ADD : test folder, where every test file are in
    	python socket communication side
    MODIF : main file to have one thread handling the socket communication

--- trunk/Gesture/Gesture.py
+++ trunk/Gesture/Gesture.py
@@ -3,6 +3,11 @@ from GestureNotificationCenter.GestureNotificationCenter import *
 from WidgetCenter.WidgetCenter import *
 import time
 import threading
+import socket
+
+HOST = 'localhost'
+PORT = 5554
+
 
 i = 0;
 
@@ -12,18 +17,29 @@ center = MoveNotificationCenter()
 
 
 def start_GestureCenter(g_center, center):
-	while (True):
-		print ""
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((HOST, PORT))
+	s.send("CONNEXION")
+	flag = 1
+	while flag == 1:
+		data = s.recv(1024)
+		l = data.split(":")
+		if (l[0] == "close"):
+			flag = 0
+		s.send('OK')
+		print l
+	s.close()
+	
 		
 
 def start_WidgetCenter(w_center, center):
 	while (True):
 		notif = center.handleNotification()
 		w_center.examinNotif(notif)
-		if notif is None:
-			print "No notification"
-		else :
-			print notif.getAssociateGestureId(), notif.getLabel()
+		#if notif is None:
+			#print ""#"No notification"
+		#else :
+			#print notif.getAssociateGestureId(), notif.getLabel()
 
 thread_a = threading.Thread(None,start_GestureCenter, None, (g_center,center,))
 thread_b = threading.Thread(None,start_WidgetCenter, None, (w_center,center,))
---
+++ trunk/test/clienttest.py
@@ -0,0 +1,12 @@
+import socket
+
+HOST = 'localhost'
+PORT = 5555
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+while 1:
+	s.send('down:10:10');
+	data = s.recv(1024)
+	print "received", repr(data)
+	s.send
+s.close()
