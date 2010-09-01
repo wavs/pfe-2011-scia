8515ced4bd1315879d76199256a42ca025fca34b
alexandre.cyprien <alexandre.cyprien@8e971d8e-9cf3-0c36-aa0f-a7c54ab41ffc>
2010-09-01 16:45:21 +0000

    alexandre cyprien ________________
    ADD : test folder, where every test file are in
    	python socket communication side
    MODIF : main file to have one thread handling the socket communication

---
+++ test/servertest.py
@@ -0,0 +1,49 @@
+import socket
+
+HOST = ""
+PORT = 5554
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+print 'Cennected by', addr
+#while 1:
+data = conn.recv(1024)
+#	if not data:break
+#	print 'Received', repr(data)
+#	conn.send(data)
+
+conn.send('down:1:10:10')
+data = conn.recv(1024)
+print "received", repr(data)
+conn.send('down:1:10:10')
+data = conn.recv(1024)
+print "received", repr(data)
+conn.send('down:1:10:10')
+data = conn.recv(1024)
+print "received", repr(data)
+conn.send('down:1:10:10')
+data = conn.recv(1024)
+print "received", repr(data)
+conn.send('down:1:10:10')
+data = conn.recv(1024)
+print "received", repr(data)
+conn.send('down:1:10:10')
+data = conn.recv(1024)
+print "received", repr(data)
+conn.send('down:1:12:12')
+data = conn.recv(1024)
+print "received", repr(data)
+conn.send('down:1:17:18')
+data = conn.recv(1024)
+print "received", repr(data)
+conn.send('down:1:19:21')
+data = conn.recv(1024)
+print "received", repr(data)
+conn.send('up:1')
+data = conn.recv(1024)
+print "received", repr(data)
+conn.send("close")
+data = conn.recv(1024)
+
+conn.close()
