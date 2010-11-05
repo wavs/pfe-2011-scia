19fd0b3412ab06644c343f353a18839d5791f0ac
creteur.c <creteur.c@8e971d8e-9cf3-0c36-aa0f-a7c54ab41ffc>
2010-11-05 03:28:23 +0000

    Ajout de la premiere version d'interfacage python globeFactory

--- trunk/GlobeFactory/bin/interface.py
+++ trunk/GlobeFactory/bin/interface.py
@@ -5,8 +5,20 @@ print "[DEBUT]"
 ge = gf.PythonGF();
 ge.Initialize();
 
+mat = ge.LoadMaterial("fun.txt");
+
+txt = ge.CreateText2D("TEST", "arial.ttf");
+ge.SetText2DColor(txt, 0.0, 1.0, 0.0)
+ge.SetText2DSize(txt, 64)
+
 run = True
 while run:
+    ge.DrawLine(0,0,800,600,1.0,0.0,0.0);
+    ge.DrawQuad(100,75,600,450,mat);
+    ge.DrawText2D(txt, 350, 525);
     run = ge.Frame();
+
+ge.DeleteText2D(txt);
+ge.UnloadMaterial(mat);
 
 print "[FIN]"
--- trunk/GlobeFactory/src/python_gf.hh
+++ trunk/GlobeFactory/src/python_gf.hh
@@ -4,6 +4,9 @@
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 
+#include <string>
+#include <vector>
+
 class GfxEngine;
 class StateHandler;
 class InputMng;
@@ -11,6 +14,7 @@ class GameMng;
 class InterfaceMng;
 class Clock;
 class FPSCounter;
+class RenderString;
 
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
@@ -24,6 +28,18 @@ class PythonGF
     void Initialize();
     bool Frame();
 
+    int LoadMaterial(const std::string& filename);
+    void UnloadMaterial(int id);
+
+    void DrawQuad(float x, float y, float w, float h, int id);
+    void DrawLine(float x1, float y1, float x2, float y2, float r = 0.5f, float g = 0.5f, float b = 0.5f);
+
+    int CreateText2D(const std::string& text, const std::string& font);
+    void DeleteText2D(int id);
+    void DrawText2D(int id, float x, float y);
+    void SetText2DColor(int id, float r, float g, float b);
+    void SetText2DSize(int id, float size);
+
   private:
     GfxEngine*    gfxEngine;
     StateHandler* stateHandler;
@@ -32,6 +48,8 @@ class PythonGF
     InterfaceMng* interfaceMng;
     Clock*        clock;
     FPSCounter*   fpsCounter;
+
+    std::vector<RenderString*> txt2D;
 };
 
 //------------------------------------------------------------------------------
